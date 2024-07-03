3cea14978d16815813c6eddfbe63e828f465fd5c
uzoefuna benson dominic <uzoefunam.benson@gmail.com>
2024-07-03 12:38:19 +0200

    Update page.tsx

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -58,7 +58,7 @@ const SignUpPage  = () => {
   };
   return (
     <>
-      <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
+      <div className="container relative flex pt-20 flex-col items-center mb-[2rem] justify-center lg:px-0">
         <div className="mx-auto  flex w-full flex-col justify-center space-y-6 sm:w-[350px] ">
           <div className="flex flex-col items-center space-y-2 text-center">
             <Icons.logo className="h-20 w-20" />
