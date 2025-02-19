6fc4342ad153eb45397da9f4463c43d9988d6083
uzoefuna benson dominic <uzoefunam.benson@gmail.com>
2025-02-19 14:07:30 +0300

    Update payload.config.ts

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -42,5 +42,19 @@ export default buildConfig({
   }),
   typescript: {
     outputFile: path.resolve(__dirname, 'payload-types.ts')
-  }
+  },
+   webpack: (config) => {  // <--- webpack config is HERE, inside buildConfig
+        return {
+            ...config,
+            resolve: {
+                fallback: {
+                    "stream": require.resolve("stream-browserify"),
+                    "url": require.resolve("url"),
+                    "querystring": require.resolve("querystring"),
+                    "path": require.resolve("path"),
+                    "fs": false, // Or require.resolve("fs") if absolutely needed
+                },
+            },
+        };
+    },
 });
